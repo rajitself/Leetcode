63e3d2d4cbbfa51cde2d94eca89db9f31d6251ed
Rahul Raj <132393359+rajitself@users.noreply.github.com>
2024-09-25 22:23:41 +0530

    Time: 47 ms (83.39%), Space: 74.5 MB (55.32%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
@@ -8,17 +8,16 @@ public:
     int lps(int i,int j,string &s,vector<vector<int>> &dp){
         if(i>j) return 0;
         if(dp[i][j]!=-1) return dp[i][j];
-        int t=-1e9,tt=-1e9;
+        
         if(s[i]==s[j]){
             if(i!=j)
-            t=2+lps(i+1,j-1,s,dp);
+            dp[i][j]=2+lps(i+1,j-1,s,dp);
             else
-            t=1+lps(i+1,j-1,s,dp);
-            
+            dp[i][j]=1+lps(i+1,j-1,s,dp);
+            return dp[i][j];
         }else{
-            tt=max(lps(i+1,j,s,dp),lps(i,j-1,s,dp));
-            
+            dp[i][j]=max(lps(i+1,j,s,dp),lps(i,j-1,s,dp));
+            return dp[i][j];
         }
-        return dp[i][j]=max(t,tt);
     }
 };
