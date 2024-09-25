d9a6525a7ff51ffc232206baf5b58e4f8bea2392
Rahul Raj <132393359+rajitself@users.noreply.github.com>
2024-09-25 22:23:26 +0530

    Time: 44 ms (85.68%), Space: 74.6 MB (55.32%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
@@ -9,14 +9,16 @@ public:
         if(i>j) return 0;
         if(dp[i][j]!=-1) return dp[i][j];
         int t=-1e9,tt=-1e9;
-        if(s[i]==s[j])
-    {
-        if(i!=j)
-        t=2+lps(i+1,j-1,s,dp);
-        else t=1+lps(i+1,j-1,s,dp);
-    }
-    else 
-        tt= max(lps(i+1,j,s,dp),lps(i,j-1,s,dp));
-return dp[i][j]=max(t,tt);
+        if(s[i]==s[j]){
+            if(i!=j)
+            t=2+lps(i+1,j-1,s,dp);
+            else
+            t=1+lps(i+1,j-1,s,dp);
+            
+        }else{
+            tt=max(lps(i+1,j,s,dp),lps(i,j-1,s,dp));
+            
+        }
+        return dp[i][j]=max(t,tt);
     }
 };
