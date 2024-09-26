09f826fa4e766e7f759bf50c4a89e15f062bd77c
Rahul Raj <132393359+rajitself@users.noreply.github.com>
2024-09-26 23:18:37 +0530

    Time: 47 ms (5.68%), Space: 49.1 MB (12.39%) - LeetHub

--- 0485-max-consecutive-ones/0485-max-consecutive-ones.cpp
+++ 0485-max-consecutive-ones/0485-max-consecutive-ones.cpp
@@ -4,11 +4,14 @@ public:
         int maxi=0,temp=0;
         for(int i=0;i<nums.size();i++){
             if(nums[i]==0){
-            maxi=max(maxi,temp);
             temp=0;
             }
-            else
+            else{
+                
             temp++;
+            if(temp>maxi)
+            maxi=temp;
+            }
         }
         return max(maxi,temp);
     }
